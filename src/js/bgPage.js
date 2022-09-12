53ea26047cabfdeb7acc4bbed4a71a70f61f3867
‘Elena <tcachelena@gmail.com>
2022-09-12 12:30:29 +0300

    feat: Сhange color on button click

--- src/js/bgPage.js
+++ src/js/bgPage.js
@@ -1,38 +1,37 @@
 export function bodyBg(body) {
-  const paginationParent = document.querySelector('.js-paginationList');
   const paginationItems = document.querySelectorAll('.js-pag-item');
+  const paginationBtns = document.querySelectorAll('.js-pagination-btn');
 
+  const colorArray = ['#c8dcdb', '#9ab5b5', '#a1acab', '#f7e7d2', '#e2ba65', '#f1d7da'];
 
-  // paginationParent.addEventListener('click', (event) => {
-  //   event.preventDefault();
-  //   const target = event.target;
+  const showPagination = (event) => {
+    event.preventDefault();
+    const target = event.target;
 
-  //   if (target.classList.contains(`active`)) {
-  //     hidePagination(target);
-  //   }
+    hidePagination();
+    changeColor();
+    target.classList.add('active');
 
-  // })
+  }
+
+  const hidePagination = () => {
+    paginationItems.forEach(item => {
+      item.classList.remove('active');
+    })
+  }
+
+  const changeColor = () => {
+    body.style.background = colorArray[Math.floor(Math.random()
+      * colorArray.length)];
+  }
 
   paginationItems.forEach(item => {
-    item.addEventListener('click', (event) => {
-      event.preventDefault();
-      const target = event.target;
-
-      showPagination(target);
-    })
+    item.addEventListener('click', showPagination);
   })
 
-  function showPagination(item) {
-    item.classList.add('active');
-
-  }
-
-  function hidePagination(item) {
-    item.classList.remove('active');
-
-  }
-
-
+  paginationBtns.forEach(item => {
+    item.addEventListener('click', changeColor);
+  })
 
 
 
