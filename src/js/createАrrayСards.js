8ff09504b9baa8afd9ce9b67d6942fdf131e4c5c
‘Elena <tcachelena@gmail.com>
2022-09-16 12:02:34 +0300

    fix: Shorten tags on mobile

--- src/js/createАrrayСards.js
+++ src/js/createАrrayСards.js
@@ -2,43 +2,43 @@ export function createCards(cardsContainer) {
   const cards = [
     {
       imageLink: 'https://images.unsplash.com/photo-1662511024026-bc935d6d7962?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60',
-      alt: 'image',
+      title: 'Happy girl',
       likes: 16,
-      date: '05/09/22',
-      tags: ['Yellow-golden-blue', 'Blue', 'Red'],
+      date: '05.09.22',
+      tags: ['Yellow-golden-blue', 'Blue-white-white', 'Red-red red'],
       author: 'John Doe',
     },
 
     {
       imageLink: 'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTF8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
-      alt: 'image',
+      title: 'Sports in yellow',
       likes: 100,
-      date: '05/09/22',
-      tags: ['Yellow', 'Blue', 'Black'],
+      date: '05.09.22',
+      tags: ['Yellow-golden-blue', 'Blue', 'Black'],
       author: 'John Doe',
     },
 
     {
       imageLink: 'https://images.unsplash.com/photo-1662515291656-304de59d70de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80',
-      alt: 'image',
+      title: 'image',
       likes: 102,
-      date: '05/09/22',
+      date: '05.09.22',
       tags: ['Yellow', 'Blue', 'Red'],
       author: 'John Doe',
     },
 
     {
       imageLink: 'https://images.unsplash.com/photo-1540331547168-8b63109225b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjR8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
-      alt: 'image',
+      title: 'image',
       likes: 12,
-      date: '05/09/22',
+      date: '05.09.22',
       tags: ['Yellow', 'Blue', 'Red'],
       author: 'John Doe',
     },
 
     {
       imageLink: 'https://images.unsplash.com/photo-1662671541709-4a3151a39409?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNzJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-      alt: 'image',
+      title: 'image',
       likes: 12,
       date: '05/09/22',
       tags: ['Yellow', 'Blue', 'Red'],
@@ -47,7 +47,7 @@ export function createCards(cardsContainer) {
 
     {
       imageLink: 'https://images.unsplash.com/photo-1662496167893-96a24461e7b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNzN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-      alt: 'image',
+      title: 'image',
       likes: 12,
       date: '05/09/22',
       tags: ['Yellow', 'Blue', 'Red'],
@@ -56,7 +56,7 @@ export function createCards(cardsContainer) {
 
     {
       imageLink: 'https://images.unsplash.com/photo-1662720215950-ce15c16373d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzODl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-      alt: 'image',
+      title: 'image',
       likes: 12,
       date: '05/09/22',
       tags: ['Yellow', 'Blue', 'Red'],
@@ -65,7 +65,7 @@ export function createCards(cardsContainer) {
 
     {
       imageLink: 'https://images.unsplash.com/photo-1662657835175-595c83a5587c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNzd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-      alt: 'image',
+      title: 'image',
       likes: 12,
       date: '05/09/22',
       tags: ['Yellow', 'Blue', 'Red'],
@@ -90,12 +90,16 @@ export function createCards(cardsContainer) {
                           <div class="info__date">
                               <time class="date js-date" datetime="2022-09-05">${card.date}</time>
                           </div>
-
-                          <p class="info__author author">
-                              by
-                              <span class="author__name">${card.author}</span>
-                          </p>
                       </div>
+
+                      <div class="info__body"> 
+                        
+                        <p class="info__author author">
+                        by
+                            <span class="author__name">${card.author}</span>
+                        </p>
+                        <h2 class="info__title title title--second title--yellow">${card.title}</h2>
+                      </div>  
 
                       <ul class="info__tags tags js-tags">
                           <li class="tags__item">
