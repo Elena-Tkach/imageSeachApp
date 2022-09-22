262cfd619cc3c19dd85276efa2f708c7348c6f0d
Elena <63470015+Elena-Tkach@users.noreply.github.com>
2022-09-22 19:35:38 +0600

    feat: Push tag list

---
+++ src/js/createFilterTagList.js
@@ -0,0 +1,19 @@
+import { getSortArrayTags } from './getSortArrayTags';
+
+export const createFilterTagList = (containerTagList) => {
+	const tagList = getSortArrayTags();
+
+
+	containerTagList.innerHTML = tagList.map(tag => {
+		return `
+			<li class="tags__item tag tag--border-yellow">
+                <a href="#" class="tags__link">
+                    <p class="text text--black text--space">${tag}</p>
+                </a>
+            </li>
+		`;
+
+	}).join('');
+
+	
+}
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 import { modalSortMenu } from './modalSortMenu';
 import { createCard } from './createСard';
 import { changeBgPage } from './changeBgPage';
+import { createFilterTagList } from './createFilterTagList';
 
 // import { createCard2 } from './createCard2';
 
@@ -9,9 +10,11 @@ const body = document.body;
 const cardsContainer = document.querySelector('.js-cards-container');
 const paginationPages = document.querySelectorAll('.js-page-btn');
 const paginationBtns = document.querySelectorAll('.js-pagination-arrow');
+const filterTagList = document.querySelector('.js-tag-list');
 
 modalSortMenu(body);
 createCard(cardsContainer);
+createFilterTagList(filterTagList);
 
 // createCard2(cardsContainer);
 
