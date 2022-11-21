7ad486ab6fc074e025022ba0abde0a7947b51c9a
‘Elena <tcachelena@gmail.com>
2022-11-21 13:53:52 +0600

    fix: Fix sorting

--- src/js/sort.js
+++ src/js/sort.js
@@ -1,16 +1,14 @@
-import { cards } from './consts';
+import { getDataFromUnsplash } from './getApiData';
 import { renderCardsList, removeCards, cardsContainer } from './cards/renderCardsList';
 
-const newCards = JSON.parse(JSON.stringify(cards));
-
-export const sortCards = () => {
+export const sortCards = async () => {
 	const sortContain = document.querySelector('.js-sort');
+	const result = await getDataFromUnsplash();
+	const copyResult = JSON.parse(JSON.stringify(result));
 
-	const sortCards = (target, dataAtribute) => {
-		const data = target.getAttribute(`${dataAtribute}`);
-
-		newCards.sort((a, b) => {
-			const dateA = new Date(a.date), dateB = new Date(b.date);
+	const sortCardsList = (data) => {
+		copyResult.sort((a, b) => {
+			const dateA = new Date(a.created_at), dateB = new Date(b.created_at);
 			switch (data) {
 				case 'date-up':
 					return dateA - dateB;
@@ -21,26 +19,74 @@ export const sortCards = () => {
 				case 'like-down':
 					return a.likes - b.likes;
 			};
+
 		});
 
-		removeCards(cardsContainer);
-		return renderCardsList(newCards);
+		return copyResult;
 	};
 
 
-	sortContain.addEventListener('click', event => {
-		const sortData = event.target.dataset['sort'];
+	sortContain.addEventListener('click', async (event) => {
+		const data = event.target.getAttribute(`data-sort`);
+		const sortCard = sortCardsList(data);
 
-		if (sortData) {
-			return sortCards(event.target, 'data-sort');
+		if (data) {
+			console.log(sortCard);
+			removeCards(cardsContainer);
+			return await renderCardsList(sortCard);
 		}
-
-		removeCards(cardsContainer);
-		return renderCardsList(newCards);
-
+		console.log(copyResult);
+		return await renderCardsList(copyResult);
 	})
-
 };
+
+
+
+
+// import { cards } from './consts';
+// import { renderCardsList, removeCards, cardsContainer } from './cards/renderCardsList';
+
+// const newCards = JSON.parse(JSON.stringify(cards));
+
+// export const sortCards = () => {
+// 	const sortContain = document.querySelector('.js-sort');
+
+// 	const sortCards = (target, dataAtribute) => {
+// 		const data = target.getAttribute(`${dataAtribute}`);
+
+// 		newCards.sort((a, b) => {
+// 			const dateA = new Date(a.date), dateB = new Date(b.date);
+// 			switch (data) {
+// 				case 'date-up':
+// 					return dateA - dateB;
+// 				case 'date-down':
+// 					return dateB - dateA;
+// 				case 'like-up':
+// 					return b.likes - a.likes;
+// 				case 'like-down':
+// 					return a.likes - b.likes;
+// 			};
+// 		});
+
+// 		removeCards(cardsContainer);
+// 		return renderCardsList(newCards);
+// 	};
+
+
+// 	sortContain.addEventListener('click', event => {
+// 		const sortData = event.target.dataset['sort'];
+
+// 		if (sortData) {
+// 			return sortCards(event.target, 'data-sort');
+// 		}
+
+// 		removeCards(cardsContainer);
+// 		return renderCardsList(newCards);
+
+// 	})
+
+// };
+
 
 
 
